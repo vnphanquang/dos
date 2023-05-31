d1b98dd701f454fdb8b32e7965f65de92ed8cbf2
Quang Phan <phan.quang@4digit.jp>
2023-05-31 23:31:15 +0900

    fix: only redirect to settings on client side

--- src/routes/settings/(contextful)/+layout.ts
+++ src/routes/settings/(contextful)/+layout.ts
@@ -1,9 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 
+import { browser } from '$app/environment';
+
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ parent }) => {
   const parentData = await parent();
-  if (!parentData.simulation) throw redirect(302, '/settings');
+  if (browser && !parentData.simulation) throw redirect(302, '/settings');
   return parentData;
 };
--- src/routes/simulation/+page.ts
+++ src/routes/simulation/+page.ts
@@ -1,9 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 
+import { browser } from '$app/environment';
+
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent }) => {
   const parentData = await parent();
-  if (!parentData.simulation) throw redirect(302, '/settings');
+  if (browser && !parentData.simulation) throw redirect(302, '/settings');
   return parentData;
 };
